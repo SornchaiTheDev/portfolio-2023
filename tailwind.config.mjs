709346ef19c22ab47026fe28a0a38911dc14ca2a
SornchaiTheDev <sornchaithedev@gmail.com>
2023-12-20 01:57:04 +0700

    Add : back to top button

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      keyframes: {
+        "slide-up": {
+          "0%": { transform: "translateY(0)" },
+          "50%": { transform: "translateY(-8px)" },
+          "100%": { transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        "slide-up": "slide-up 2s infinite ease-in-out",
+      },
+    },
+  },
+  plugins: [],
+};
